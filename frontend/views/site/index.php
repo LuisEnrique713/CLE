94b37b91eefe835023464d9e6ad26e47ca2c75c3
Luis Tec <luis.teccupul@itsva.edu.mx>
2022-12-06 11:26:21 -0600

    test

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -12,7 +12,7 @@ $this->title = 'CLE';
 
 
 </center>
- <!-- ==== FONDO ==== -->  
+ <!-- ==== FONDOS ==== -->  
 <style>
 body {
   background-image: url('https://static.wixstatic.com/media/a6f1e7_7dc1568d62734a679dbc27e7858f6006~mv2_d_5184_3456_s_4_2.jpg/v1/fill/w_1920,h_1280,al_c,q_90,usm_0.66_1.00_0.01/a6f1e7_7dc1568d62734a679dbc27e7858f6006~mv2_d_5184_3456_s_4_2.jpg');
